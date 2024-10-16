40f562ba855403a8fc7672c924a835af522f94f5
AnthorNet <anthor.net@gmail.com>
2024-10-16 13:08:18 +0200

    Let's get on it!

---
+++ CoinPusher.ino
@@ -0,0 +1,14 @@
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("Starting Coin Pusher");
+    delay(2000);
+}
+
+/**
+ *  LOOP AT THE END SO EVERY VARIABLES ARE USABLE...
+ */
+void loop()
+{
+
+}
